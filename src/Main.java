a162b7a4cd853caf0b0385ba754b2901f06f8c5f
AveIvan94 <a.iwanowska@icloud.com>
2022-03-09 19:41:56 +0100

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String name = scanner.nextLine();
+        System.out.println("Jak masz na nazwisko? ");
+        String lastName = scanner.nextLine();
+
+        System.out.println();
+
+    }
 }
